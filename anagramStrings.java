3bb31643c9acbedc1e1b4f2d2b008b96ff634ed6
SPN288 <spnirala288@gmail.com>
2024-07-18 17:27:43 +0530

    added

---
+++ anagramStrings.java
@@ -0,0 +1,33 @@
+
+import java.util.HashMap;
+
+/*
+ Write a function to validate if the provided two strings are anagrams or not.
+  If the two strings are anagrams, then return ‘yes’. Otherwise, return ‘no’.
+ */
+
+public class anagramStrings {
+    
+    static String checkanagram(String s1,String s2){
+        String st1=s1.toLowerCase();
+        String st2=s2.toLowerCase();
+
+        HashMap<Character,Character> stcheck=new HashMap<>();
+
+        for (int i = 0; i < st1.length(); i++) {
+            stcheck.put(st1.charAt(i), st1.charAt(i));
+        }
+        for (int i = 0; i < st2.length(); i++) {
+            if(!stcheck.containsValue(st2.charAt(i))){
+                return "no";
+            }
+        }
+
+        
+        
+        return "yes";
+    }
+    public static void main(String[] args) {
+        System.out.println(checkanagram("Listen","Silent"));
+    }
+}

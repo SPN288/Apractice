41d5fb49b9131eb512532dc41d81cfac56473efd
SPN288 <spnirala288@gmail.com>
2024-07-25 22:00:09 +0530

    add

--- nearestSmallerTower.java
+++ nearestSmallerTower.java
@@ -1,22 +1,50 @@
+import java.util.ArrayList;
+
 public class nearestSmallerTower {
 
-   static int[] smallesttower(int[] arr){
-    int[] res=new int[arr.length];
+//    static int[] smallesttower(int[] arr){
+//     int[] res=new int[arr.length];
     
+//     for (int i = 0; i < arr.length; i++) {
+//         int min=i;
+//         for (int j = 0; j < arr.length; j++) {
+//             if(i==j){continue;}
+//             if(arr[j]<arr[min]){
+//                 min=j;
+//             }else if(arr[j]==arr[min]&&Math.abs(i-min)>Math.abs(i-j)){
+//                 min=j;
+//             }
+//         }
+//         res[i]=min;
+//         if(res[i]==i){
+//             res[i]=-1;
+//         }
+//     }
+    
+//     return res;
+//    }
+
+static int[] smallesttower(int[] arr){
+    int[] res=new int[arr.length];
+    ArrayList<Integer> ll=new ArrayList<>();
+    int min=0;
     for (int i = 0; i < arr.length; i++) {
-        int min=i;
-        for (int j = 0; j < arr.length; j++) {
-            if(i==j){continue;}
-            if(arr[j]<arr[min]){
-                min=j;
-            }else if(arr[j]==arr[min]&&Math.abs(i-min)>Math.abs(i-j)){
-                min=j;
-            }
+        if(arr[i]<arr[min]){min=i;}
+    }
+    //System.out.println("samall->"+min);
+    for (int i = 0; i < arr.length; i++) {
+        if(arr[i]==arr[min]){ll.add(i);}
+    }
+    //System.out.println(ll);
+    for (int i = 0; i < arr.length; i++) {
+        res[i]=i;
+        for (int j = 0; j < ll.size(); j++) {
+
+            res[i]=Math.min(res[i],Math.abs(i-ll.get(j)));
         }
-        res[i]=min;
-        if(res[i]==i){
-            res[i]=-1;
-        }
+        // if(res[i]==i){
+        //                res[i]=-1;
+        //           }
     }
     
     return res;
---
+++ removeelement.java
@@ -0,0 +1,28 @@
+public class removeelement {
+    static void remove(int[] arr , int x){
+        int inx=0;
+        int l=arr.length;
+        for (int i = 0; i < l; i++) {
+            if(arr[i]==x){inx=i;}
+        }
+        for (int i = inx; i < l-1; i++) {
+            
+            arr[i]=arr[i+1];
+        }
+        arr[l-1]=0;
+    }
+    static void printer(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+       }
+
+    public static void main(String[] args) {
+        int [] arr1={1,2,3,4,5,6,7,8};
+        printer(arr1);
+        remove(arr1,3);
+        printer(arr1);
+    }
+    
+}

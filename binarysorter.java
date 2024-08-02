235dbfe2ab84e07f6e5aa371503d5e48bcf4e2a9
SPN288 <spnirala288@gmail.com>
2024-08-03 05:29:25 +0530

    add

--- binarysorter.java
+++ binarysorter.java
@@ -1,28 +1,6 @@
 public class binarysorter {
 
-    // public static String hot(String s){
-        
-    //     StringBuilder sb=new StringBuilder();
-    //     for (int i = 0; i < s.length();i++) {
-    //         sb.append(s.charAt(i));
-        
-    //     }
-    //     int x=sb.length();
-    //     for(int j=0;j<x;j++){
-    //         for (int i = 0; i < sb.length()-1;) {
-            
-    //             if(sb.charAt(i)=='1'&&sb.charAt(i+1)=='0'){
-    //                 sb.deleteCharAt(i);
-    //                 i++;
-    //             }else{
-    //             i++;}
-    //         }
-    //     }
-        
-
-    //     return sb.toString();
-        
-    // }
+    
     public static String hot(String s){
         
         StringBuilder sb=new StringBuilder();
@@ -31,8 +9,12 @@ public class binarysorter {
         
         }
         int size=sb.length()-1;
+        int p=size;
         while (size!=0) { 
-            if(sb.charAt(size)=='0'&&sb.charAt(size-1)=='1'){
+            if(size<p&& sb.charAt(size)=='0'&&sb.charAt(size-1)=='1'&&sb.charAt(size+1)=='0'){
+                sb.deleteCharAt(size+1);
+            }
+            else if(sb.charAt(size)=='0'&&sb.charAt(size-1)=='1'){
                 sb.deleteCharAt(size-1);
             }else{size--;}
         }

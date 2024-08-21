62b805f5f52ed99e97fc9a2d96a19b8e6316f428
SPN288 <spnirala288@gmail.com>
2024-08-21 23:10:08 +0530

    lol

--- binaryOperation.java
+++ binaryOperation.java
@@ -38,6 +38,25 @@ public class binaryOperation {
         return ans;
 
     }
+    public static int operationsBinaryString (String str)
+  {
+    if (str == null)
+      return -1;
+    int res = str.charAt (0) - '0';
+    for (int i = 1; i < str.length ();)
+      {
+	char prev = str.charAt (i);
+	  i++;
+	if (prev == 'A')
+	  res = res & (str.charAt (i) - '0');
+	else if (prev == 'B')
+	  res = res | (str.charAt (i) - '0');
+	else
+	    res = res ^ (str.charAt (i) - '0');
+	  i++;
+      }
+    return res;
+  }
 
     public static void main(String[] args) {
         System.out.println("Answer=" + calcu("1C0C1C1A0B1"));//1

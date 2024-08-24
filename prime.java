ba7d62e0fb5192552dda2bfb5a1553ff1b8e7ec0
SPN288 <spnirala288@gmail.com>
2024-08-24 21:39:57 +0530

    lol

---
+++ prime.java
@@ -0,0 +1,15 @@
+public class prime {
+    static boolean isPrime(int  num){
+        
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if(num%i==0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(12));
+    }
+}

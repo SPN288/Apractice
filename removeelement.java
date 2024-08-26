3bbdc395c02ce126c2e95638186b143cade36eb3
SPN288 <spnirala288@gmail.com>
2024-08-26 21:44:33 +0530

    lol

--- removeelement.java
+++ removeelement.java
@@ -21,7 +21,7 @@ public class removeelement {
     public static void main(String[] args) {
         int [] arr1={1,2,3,4,5,6,7,8};
         printer(arr1);
-        remove(arr1,3);
+        remove(arr1,4);
         printer(arr1);
     }
     

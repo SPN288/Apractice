3ec7e045b734f1edfcdd8c1be8aa85ee3da20b6b
SPN288 <spnirala288@gmail.com>
2024-08-28 10:47:39 +0530

    lol

---
+++ cocoNutCount.java
@@ -0,0 +1,19 @@
+public class cocoNutCount {
+    static int cocoNut(int boxSize,int need,int days){
+        int ans=0;
+        int x=need*days;
+        if(x%boxSize==0){
+            ans=x/boxSize;
+        }else{
+            ans=1+x/boxSize;
+        }
+
+
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(cocoNut(16, 2, 8));
+    }
+}

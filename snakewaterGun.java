a4f9ff61c609c98d81c79ccfe5c1ba04a00df033
SPN288 <spnirala288@gmail.com>
2024-09-02 10:32:49 +0530

    day

--- snakewaterGun.java
+++ snakewaterGun.java
@@ -4,34 +4,57 @@ public class snakewaterGun {
         int s1=0;
         int s2=0;
         for (int i = 0; i < s.length(); ) {
-             if(s.charAt(i)=='s'){
-                i+=5;
-                if(s.charAt(i)=='w'){
-                    s1++;
+            switch (s.charAt(i)) {
+                case 's' -> {
                     i+=5;
-                }else if(s.charAt(i)=='g'){
-                    s2++;
+                    switch (s.charAt(i)) {
+                        case 'w' -> {
+                            s1++;
+                            i+=5;
+                        }
+                        case 'g' -> {
+                            s2++;
+                            i+=3;
+                        }
+                        default -> {
+                        }
+                    }
+                }
+
+
+                case 'w' -> {
+                    i+=5;
+                switch (s.charAt(i)) {
+                    case 'g' -> {
+                        s1++;
+                        i+=3;
+                    }
+                    case 's' -> {
+                        s2++;
+                        i+=5;
+                    }
+                    default -> {
+                    }
+                }
+                }
+                case 'g' -> {
                     i+=3;
-                }else{}
-             }else if(s.charAt(i)=='w'){
-                i+=5;
-                if(s.charAt(i)=='g'){
-                    s1++;
-                    i+=3;
-                }else if(s.charAt(i)=='s'){
-                    s2++;
-                    i+=5;
-                }else{}
-             }else if(s.charAt(i)=='g'){
-                i+=3;
-                if(s.charAt(i)=='w'){
-                    s2++;
-                    i+=5;
-                }else if(s.charAt(i)=='s'){
-                    s1++;
-                    i+=5;
-                }else{}
-             }
+                switch (s.charAt(i)) {
+                    case 'w' -> {
+                        s2++;
+                        i+=5;
+                    }
+                    case 's' -> {
+                        s1++;
+                        i+=5;
+                    }
+                    default -> {
+                    }
+                }
+                }
+                default -> {
+                }
+            }
         }
 
         if(p==1){return s1;}else{return s2;}

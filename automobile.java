37a2889e44d8fe27ece1b7a37b594d79dc2a0eee
SPN288 <spnirala288@gmail.com>
2024-09-19 17:54:34 +0530

    day

--- automobile.java
+++ automobile.java
@@ -2,17 +2,23 @@
 import java.util.Scanner;
 
 public class automobile {
-    static void countVehicle(int V,int W){
-        int y=(4*V-W)/2;
-        int x=V-y;
-        System.out.println("FW="+x+" TW="+y);
+
+    static void countVehicle(int V, int W) {
+        int y = (4 * V - W) / 2;
+        int x = V - y;
+        if (W >= 2 && (W % 2 == 0) && V < W) {
+            System.out.println("FW=" + x + " TW=" + y);
+        } else {
+            System.out.println("Invalid Input");
+        }
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter total no. of vehicles");
-        int V=sc.nextInt();
+        int V = sc.nextInt();
         System.out.println("Enter total no. of wheels");
-        int W=sc.nextInt();
+        int W = sc.nextInt();
         countVehicle(V, W);
     }
 }

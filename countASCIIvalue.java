fdcfd751007d49706e394ab9de51a5ca965e6d64
SPN288 <spnirala288@gmail.com>
2024-09-23 10:33:49 +0530

    day

---
+++ countASCIIvalue.java
@@ -0,0 +1,10 @@
+public class countASCIIvalue {
+    static int countASCII(char c){
+        int ans=c;
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(countASCII('A'));
+        System.out.println(countASCII('e'));
+    }
+}

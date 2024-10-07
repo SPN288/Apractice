0e3c289e8ba38a9249f4ee0ba5ca110a0658edc0
SPN288 <spnirala288@gmail.com>
2024-10-07 07:37:16 +0530

    day

--- digitcheck.java
+++ digitcheck.java
@@ -1,16 +1,19 @@
 public class digitcheck {
     public static void main(String[] args) {
-        char a='a';char b='3';
-        String s="satya123Prakash";
-        System.out.println(Character.isDigit(a));
-        System.out.println(Character.isDigit(b));
-        System.out.println(Character.isDigit(b));
+        // char a='a';char b='3';
+        // String s="satya123Prakash";
+        // System.out.println(Character.isDigit(a));
+        // System.out.println(Character.isDigit(b));
+        // System.out.println(Character.isDigit(b));
 
-        for (int i = 0; i < s.length(); i++) {
-            System.out.println(s.charAt(i)+"="+ Character.isDigit(s.charAt(i)));
-        }
-        String x="satya123";
-        int p=Character.getNumericValue(x.charAt(6));
-        System.out.println(p);
+        // for (int i = 0; i < s.length(); i++) {
+        //     System.out.println(s.charAt(i)+"="+ Character.isDigit(s.charAt(i)));
+        // }
+        // String x="satya123";
+        // int p=Character.getNumericValue(x.charAt(6));
+        // System.out.println(p);
+        String j="012345";
+        int g=Integer.parseInt(j);
+        System.out.println(g);
     }
 }

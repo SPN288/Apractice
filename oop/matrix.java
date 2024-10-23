8f77c034f4c3d4087be63d67c72983777567dbe0
SPN288 <spnirala288@gmail.com>
2024-10-23 12:24:12 +0530

    day

---
+++ oop/matrix.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class matrix {
+
+   public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3},
+                       {4, 5, 6},
+                       {7,8,9}};
+        System.out.println(arr[2][0]);
+
+    }
+}

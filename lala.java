6dc990772288dd2ef2155c8f8d92ffd0b3a88cdb
SPN288 <spnirala288@gmail.com>
2024-11-01 09:37:17 +0530

    day

--- lala.java
+++ lala.java
@@ -2,5 +2,7 @@ public class lala {
     public static void main(String[] args) {
         System.out.println("hulululululululu");
         System.out.println("hulululululululu");
+        System.out.println("hulululululululu");
+        System.out.println("hulululululululu");
     }
 }

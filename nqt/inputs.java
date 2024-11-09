ca61e68bb9bf4ba8fa4600b91f4c5a47639bdaa9
SPN288 <spnirala288@gmail.com>
2024-11-09 15:14:50 +0530

    day

--- nqt/inputs.java
+++ nqt/inputs.java
@@ -15,27 +15,7 @@ public class inputs {
         System.out.println();
     }
 
-    public static boolean isSubset(String w1,String w2){
-        int[]s1=new int[26];
-        int[]s2=new int[26];
-        for(int i=0;i<w1.length();i++){
-            s1[(int)w1.charAt(i)-97]++;
-        }
-        print(s1);
-        for(int i=0;i<w2.length();i++){
-            s2[(int)w2.charAt(i)-97]++;
-        }
-        print(s2);
-
-        for(int i=0;i<w2.length();i++){
-            
-            if(s1[(int)w2.charAt(i)-97]<s2[(int)w2.charAt(i)-97]){
-                return false;
-            }
-        }
-        return true;
-
-    }
+    
 
     public static void main(String[] args) {
         
